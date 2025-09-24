6b2f8697bb86862f6eb9eab8befa030540ee25bb
Naveen Kumar <144909719+naveen7413@users.noreply.github.com>
2025-09-24 15:05:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Skills from './components/Skills';
-import Testimonials from './components/Testimonials';
+import Testimonials from './components/Testimonials' ;
 
 function App() {
   return (
